71c0f30ebbb933ed3d9d898a9d364f8216b615c4
Timothy <timothy.nathanael2307@gmail.com>
2020-04-22 20:49:40 +0700

    override navbar lama di selain index jadi sama kaya di index. bikin konsistensi bahasa. bikin button buat carousel.

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,32 +38,32 @@ function next(){
 function prev(){
     
     stopTrigger();
-    if(carouselCount==0)carouselCount=totalCarousel;
-    else carouselCount-=1;
+    carouselCount-=1;
+    if(carouselCount==0) carouselCount=totalCarousel;
     // console.log("prev"+" "+carouselCount)
     changeImg();
     carouselTrigger();
 }
 
 function content1(){
-    let title = "Our Company";
-    let contentx = "KYamedia is our company name. Our Company is one of many companies that sells books. There are two kinds of books that we sell. The first is paper book and the second one is electronic book.";
+    let title = "Perusahaan Kami";
+    let contentx = "Nama perusahaan kami adalah KYAMedia. Perusahaan kami adalah salah satu perusahaan yang menjual bermacam-macam buku. Terdapat dua macam buku yang kami jual, buku kertas, dan buku elektronik.";
     $(".about-title").html(title);
     $(".about-isi").html(contentx);
     
 }
 
 function content2(){
-    let title = "Our History";
-    let contentx = "The History of our company started back March 2020 when our CEO thought of starting a book selling company. It doesn't take long to make the company big. Because of the diligence of the CEO and the power of togetherness of the employeess, our company has become the greatest company in indonasei.";
+    let title = "Sejarah Kami";
+    let contentx = "Perusahaan kami terbentuk pada bulan Maret 2020. CEO kami memiliki visi untuk meningkatkan angka literasinya. oleh karena itu, CEO kami membuat toko buku online sehingga memudahkan orang untuk membeli tanpa harus pergi. ide CEO kami didapat saat ia kesulitan mencari sebuah buku di toko online dan harus pergi ke toko fisik. hal tersebut membuatnya merasa tidak nyaman. ia tidak ingin orang lain memiliki pengalaman kurang menyenangkan tersebut.";
     $(".about-title").html(title);
     $(".about-isi").html(contentx);
     
 }
 
 function content3(){
-    let title = "Our Stores";
-    let contentx = "We has many stores and outlets scattered in Indonasei. We have over 100 stores in Java Island and Over 200 stores all around Indonasei.";
+    let title = "Toko Kami";
+    let contentx = "Kami memiliki banyak toko dan agen yang tersebar di seluruh Indonesia. Kami memiliki lebih dari 100 toko di pulau jawa dan lebih dari 200 toko di seluruh indonesia.";
     $(".about-title").html(title);
     $(".about-isi").html(contentx);
     
